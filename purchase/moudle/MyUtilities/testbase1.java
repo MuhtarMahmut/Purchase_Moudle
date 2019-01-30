55e8e9d933bd0a229b426f33c5c83577b66b63ff
MuhtarMahmut <Muhtar.Mahmut0519@yahoo.com>
2019-01-30 13:08:59 -0500

    testbase

--- purchase/moudle/MyUtilities/testbase1.java
+++ purchase/moudle/MyUtilities/testbase1.java
@@ -1,19 +1,16 @@
 package MyUtilities;
-import com.github.javafaker.Faker;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 import java.util.concurrent.TimeUnit;
 
 public class testbase1 {
 	
 	public static WebDriverWait wait;
     public static WebDriver driver;
-    public static Faker faker;
     public static myLibrary mylib;
 
     @BeforeMethod
@@ -21,7 +18,6 @@ public class testbase1 {
 
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        faker = new Faker();
         mylib=new myLibrary(driver);
         driver.manage().window().maximize();
         wait=new WebDriverWait(driver, 10);
